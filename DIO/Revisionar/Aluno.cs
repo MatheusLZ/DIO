effa1959a65cf42f6da6b808d95497b595a02c82
MatheusL <80010709+MatheusLZ@users.noreply.github.com>
2022-01-25 17:51:35 -0300

    Tentativa de continuação do programa
    Falha na execução por um erro desconhecido por minha pessoa no Console.ReadLine, tal qual acusa que há tentativa de implicitamente mudar "string" para "int", quando o que quero é um string
    Adoraria uma luz para com esta tarefa, visto que mesmo testando o código todo de Gabriel Faraday ainda assim acusam-se falhas (graves, inclusive...o problema está no meu Visual Studio?)
    
    link para o repositório da aula:
    https://github.com/gabrielfaraday/csharp-examples/tree/master/Revisao

---
+++ DIO/Revisionar/Aluno.cs
@@ -0,0 +1,10 @@
+namespace Revisionar
+{
+    public struct Aluno
+    {
+        
+        public int Nome { get; set; }
+
+        public decimal Nota { get; set; }
+    }
+}
--- DIO/Revisionar/Program.cs
+++ DIO/Revisionar/Program.cs
@@ -6,6 +6,8 @@
 
             static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
             string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "x")
@@ -13,13 +15,34 @@
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //to do: adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                    
+
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        
                         break;
                     case "2":
-                        //listar alunos
+                        foreach(var a in alunos){
+                            Console.WriteLine($"ALUNO: {a.Nome} - NOTA {a.Nota}");
+                        }
                         break;
                     case "3":
-                        //calcular média geral
+                        //calcular média geral//
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
@@ -33,6 +56,7 @@
 
         private static string ObterOpcaoUsuario()
         {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada aqui");
             Console.WriteLine("1 - Inserir aluno");
             Console.WriteLine("2 - Listar alunos");
