5bef30792709e89d83f283238ebe548355a9186e
MatheusL <80010709+MatheusLZ@users.noreply.github.com>
2022-01-25 16:13:51 -0300

    Passo 2

--- DIO/Revisionar/Program.cs
+++ DIO/Revisionar/Program.cs
@@ -4,42 +4,47 @@
 
         class Program{
 
-            static void Main(string[] args){
+            static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-                Console.WriteLine("Informe a opção desejada aqui");
-                Console.WriteLine("1 - Inserir aluno");
-                Console.WriteLine("2 - Listar alunos");
-                Console.WriteLine("3 - Calcular média geral");
-                Console.WriteLine("x - Sair");
-                Console.WriteLine();
-
-                string opcaoUsuario = Console.ReadLine();
-
-                while (opcaoUsuario.ToUpper() != "x"){
-                    switch(opcaoUsuario){
-                        case "1":
-                            //to do: adicionar aluno
-                            break;
-                        case "2":
-                            //listar alunos
-                            break;
-                        case "3":
-                            //calcular média geral
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
-
-                    Console.WriteLine("Informe a opção desejada aqui");
-                    Console.WriteLine("1 - Inserir aluno");
-                    Console.WriteLine("2 - Listar alunos");
-                    Console.WriteLine("3 - Calcular média geral");
-                    Console.WriteLine("x - Sair");
-                    Console.WriteLine();
-
-                    opcaoUsuario = Console.ReadLine();
+            while (opcaoUsuario.ToUpper() != "x")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //to do: adicionar aluno
+                        break;
+                    case "2":
+                        //listar alunos
+                        break;
+                    case "3":
+                        //calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
                 }
 
+                
+                opcaoUsuario = ObterOpcaoUsuario();
             }
+
         }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada aqui");
+            Console.WriteLine("1 - Inserir aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("x - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+           
+            Console.WriteLine();
+
+            return opcaoUsuario;
+        }
+    }
     }
